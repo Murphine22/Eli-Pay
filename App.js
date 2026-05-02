b114753512266e1e27663cab8cc14ea86f83788c
Elisha Ejimofor <elishaejimofor@gmail.com>
2026-05-02 12:16:36 +0100

    Updated my .env file

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './Config/dbConfig.js'
+
+dotenv.config()
 
 const app = express()
+
+// Middleware
+app.use(express.json())
+
+// Connect to MongoDB
+connectDB()
 
 app.get('/hello', (req, res) => {
   res.send('Hello')
 })
 
+const PORT = process.env.PORT || 5000
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
